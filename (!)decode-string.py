249d7224796f79861675c1a7e2416f2d96b727f8
MX1MR41 <107861045+MX1MR41@users.noreply.github.com>
2022-12-25 11:23:05 +0300

    Create (!)decode-string.py

---
+++ (!)decode-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        n, curr, nums, strs = 0, [], [], []
+        for c in s:
+            if c.isalpha():
+                curr.append(c)
+            elif c.isdigit():
+                n = n*10 + ord(c)-ord('0')
+            elif c == '[':
+                nums.append(n)
+                strs.append(curr)
+                n, curr = 0, []
+            elif c == ']':
+                strs[-1].extend(curr*nums.pop())
+                curr = strs.pop()
+        return "".join(curr)
+     
