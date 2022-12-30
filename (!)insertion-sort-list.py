636d9883a45b3f0f75256a6ec0eaceb0df2d0fec
MX1MR41 <107861045+MX1MR41@users.noreply.github.com>
2022-12-30 20:19:12 +0300

    Create (!)insertion-sort-list.py

---
+++ (!)insertion-sort-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def insertionSortList(self, head):
+        if head is None or self.isSorted(head):
+            return head
+
+        dummy = ListNode(-2147483648)
+        dummy.next = head
+        cur, sorted_tail = head.next, head
+        while cur:
+            prev = dummy
+            while prev.next.val < cur.val:
+                prev = prev.next
+            if prev == sorted_tail:
+                cur, sorted_tail = cur.next, cur
+            else:
+                cur.next, prev.next, sorted_tail.next = prev.next, cur, cur.next
+                cur = sorted_tail.next
+
+        return dummy.next
+
+    def isSorted(self, head):
+        while head and head.next:
+            if head.val > head.next.val:
+                return False
+            head = head.next
+        return True
