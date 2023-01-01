217359e2fbcf2de26359a2c50994ee64c8774e7b
MX1MR41 <107861045+MX1MR41@users.noreply.github.com>
2023-01-01 11:51:58 +0300

    Create (!)last-stone-weight.py

---
+++ (!)last-stone-weight.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        max_heap = [-x for x in stones]
+        heapq.heapify(max_heap)
+        for i in xrange(len(stones)-1):
+            x, y = -heapq.heappop(max_heap), -heapq.heappop(max_heap)
+            heapq.heappush(max_heap, -abs(x-y))
+        return -max_heap[0]
