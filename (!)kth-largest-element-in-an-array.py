b6871b5bbc4d81456ed67ec9138e1c289eae043f
MX1MR41 <107861045+MX1MR41@users.noreply.github.com>
2023-01-01 11:47:55 +0300

    Create (!)kth-largest-element-in-an-array.py

---
+++ (!)kth-largest-element-in-an-array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        def nth_element(nums, n, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+
+            left, right = 0, len(nums)-1
+            while left <= right:
+                pivot_idx = randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
+
+        nth_element(nums, k-1, compare=lambda a, b: a > b)
+        return nums[k-1]
