317c8b2d36e077e585792be26a6568253be4667d
MX1MR41 <107861045+MX1MR41@users.noreply.github.com>
2023-01-09 10:30:58 +0300

    Create (!)find-pivot-index.py

---
+++ (!)find-pivot-index.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        left_sum = 0
+        for i, num in enumerate(nums):
+            if left_sum == (total-left_sum-num):
+                return i
+            left_sum += num
+        return -1
