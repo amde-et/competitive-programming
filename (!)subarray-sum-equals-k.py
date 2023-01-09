61d1074c43ff624412f7a7e81e1fd2238891c6a7
MX1MR41 <107861045+MX1MR41@users.noreply.github.com>
2023-01-09 10:33:48 +0300

    Create (!)subarray-sum-equals-k.py

---
+++ (!)subarray-sum-equals-k.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        result = 0
+        accumulated_sum = 0
+        lookup = collections.defaultdict(int)
+        lookup[0] += 1
+        for num in nums:
+            accumulated_sum += num
+            result += lookup[accumulated_sum - k]
+            lookup[accumulated_sum] += 1
+        return result
